0124b8719fb0a4be01299630ee2086bd77d4b1e3
PenguinAlex <sedik11032002@gmail.com>
2023-01-25 21:57:15 +0300

    first commit

---
+++ calculator/Calc.cs
@@ -0,0 +1,80 @@
+﻿using NLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    //класс, реализующий интерфейс InterfaceCalc
+    public class Calc : InterfaceCalc
+    {
+        private double a = 0;
+        private string localpath = "C:\\Projects\\C#\\calculator\\calculator\\CalcFunctions.lua";
+
+        public void Put_A(double a)
+        {
+            this.a = a;
+        }
+
+        public void Clear_A()
+        {
+            a = 0;
+        }
+
+        public double Multiplication(double b)
+        {
+            return a * b;
+        }
+
+        public double Division(double b)
+        {
+            return a / b;
+        }
+
+        public double Sum(double b)
+        {
+            return a + b;
+
+        }
+
+        public double Subtraction(double b) 
+        {
+            return a - b;
+        }
+
+        public double DegreeY(double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Sqrt()
+        {
+            return Math.Sqrt(a);
+        }
+
+        public double Square()
+        {
+            return Math.Pow(a, 2.0);
+        }
+
+        public double Factorial()
+        {
+            using (Lua luaState = new Lua())
+            {
+                luaState.DoFile(localpath);
+                
+                LuaFunction factorial = luaState["fact"] as LuaFunction;
+                var result = Convert.ToDouble(factorial.Call(a)[0]);
+                Console.WriteLine(result);
+                return Convert.ToDouble(result);
+            }
+        }
+
+        public double Ckn(double k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ calculator/CalcFunctions.lua
@@ -0,0 +1,9 @@
+function fact(a)
+    local f = 1;
+    for i = 1, a, 1 do
+      f = f * i;
+    end;
+  
+    return f;
+end
+
